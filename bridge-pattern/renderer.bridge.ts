4053b7a7540094560a7917b6dff13999668a9383
jai <jai7a7@gmail.com>
2025-01-12 02:04:53 +0530

    fix formatting

--- bridge-pattern/renderer.bridge.ts
+++ bridge-pattern/renderer.bridge.ts
@@ -1,13 +1,13 @@
 import { IRenderingEngine } from "./types.ts";
 
 export class WebRenderingEngine implements IRenderingEngine {
-    renderElement(element: string, props: Record<string, unknown>): void {
-        console.log(`${element} rendered to web with props ${JSON.stringify(props)}`)
-    }
+  renderElement(element: string, props: Record<string, unknown>): void {
+    console.log(`${element} rendered to web with props ${JSON.stringify(props)}`);
+  }
 }
 
 export class AndroidRenderingEngine implements IRenderingEngine {
-    renderElement(element: string, props: Record<string, unknown>): void {
-        console.log(`${element} rendered to android with props ${JSON.stringify(props)}`)
-    }
+  renderElement(element: string, props: Record<string, unknown>): void {
+    console.log(`${element} rendered to android with props ${JSON.stringify(props)}`);
+  }
 }
--- bridge-pattern/usage.ts
+++ bridge-pattern/usage.ts
@@ -1,4 +1,4 @@
-// emulates how modern UI frameworks handle platform-specific rendering. 
+// emulates how modern UI frameworks handle platform-specific rendering.
 // The abstraction (UI components) remains consistent while the implementation (rendering logic) varies by platform.
 
 import { WebRenderingEngine } from "./renderer.bridge.ts";
@@ -6,18 +6,18 @@ import { IComponent, IRenderingEngine } from "./types.ts";
 
 // concrete abstraction
 class Button implements IComponent {
-    name: string
-    private engine: IRenderingEngine
-    constructor(engine: IRenderingEngine){
-        this.name = "button"
-        this.engine = engine
-    }
-    render(props: Record<string,unknown>): void {
-        this.engine.renderElement(this.name, props)
-    }
+  name: string;
+  private engine: IRenderingEngine;
+  constructor(engine: IRenderingEngine) {
+    this.name = "button";
+    this.engine = engine;
+  }
+  render(props: Record<string, unknown>): void {
+    this.engine.renderElement(this.name, props);
+  }
 }
 
 // usage
-const engine = new WebRenderingEngine()
-const button = new Button(engine)
-button.render({color: "blue", size: "small"})
+const engine = new WebRenderingEngine();
+const button = new Button(engine);
+button.render({ color: "blue", size: "small" });
