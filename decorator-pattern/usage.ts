c33c34ac8c43df4f1e08422f44aa3e31fb581ca1
jai <jai7a7@gmail.com>
2025-01-24 01:28:39 +0530

    fmt

--- decorator-pattern/usage.ts
+++ decorator-pattern/usage.ts
@@ -1,21 +1,23 @@
-import { measure } from "./measure.decorator.ts"
+import { required, validate } from "./class-validator.decorator.ts";
+import { measure } from "./measure.decorator.ts";
 
+// Example 1 - Using method decorator
 class Database {
-    private name: string
-    constructor(name: string){
-        this.name = name
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  @measure("debug")
+  query(sql: string) {
+    for (let i = 0; i < 10e6; i++) {
+      continue;
     }
-    @measure("debug")
-    query(sql: string) {
-        for(let i = 0; i < 10e6; i++){
-            continue
-        }
-        console.log("query executed: "+ sql)
-    }
+    console.log("query executed: " + sql);
+  }
 }
 
 const db = new Database("postgresql");
-db.query("SELECT * FROM USERS;")
+db.query("SELECT * FROM USERS;");
 /**
 entering fn: 'query' | params: SELECT * FROM USERS; | 93.522292
 query executed: SELECT * FROM USERS;
