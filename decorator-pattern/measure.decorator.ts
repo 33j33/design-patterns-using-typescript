cf38465635da641bf542fe1d06908dd66fd84f0c
jai <jai7a7@gmail.com>
2025-01-24 01:28:07 +0530

    fmt

--- decorator-pattern/measure.decorator.ts
+++ decorator-pattern/measure.decorator.ts
@@ -1,19 +1,19 @@
-// method decorator   
-export function measure(mode?: "debug" ){
- // deno-lint-ignore ban-types
- return function (target: Function, context: ClassMethodDecoratorContext) {
-  if (context.kind == "method") {
-    return function (...args: unknown[]) {
-      const debug = mode === "debug";
-      const name = context.name;
-      const start = performance.now();
-      debug && console.log(`entering fn: '${String(name)}' | params: ${args} | ${start}`)
-      const result = target.apply(this, args);
-      const end = performance.now();
-      debug && console.log(`existing fn: '${String(name)}' | params: ${args} | ${end}`)
-      console.log(`execution time for '${String(name)}' fn: ${end - start} ms`);
-      return result;
-    };
-  }
-}
+// method decorator
+export function measure(mode?: "debug") {
+  // deno-lint-ignore ban-types
+  return function (target: Function, context: ClassMethodDecoratorContext) {
+    if (context.kind == "method") {
+      return function (...args: unknown[]) {
+        const debug = mode === "debug";
+        const name = context.name;
+        const start = performance.now();
+        debug && console.log(`entering fn: '${String(name)}' | params: ${args} | ${start}`);
+        const result = target.apply(this, args);
+        const end = performance.now();
+        debug && console.log(`existing fn: '${String(name)}' | params: ${args} | ${end}`);
+        console.log(`execution time for '${String(name)}' fn: ${end - start} ms`);
+        return result;
+      };
+    }
+  };
 }
